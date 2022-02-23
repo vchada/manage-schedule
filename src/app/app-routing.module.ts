3de8859d29a93a778361263bf55b18d140fc8593
Sourabh Agrawat <Sourabh.Agrawat7@T-Mobile.com>
2022-02-23 23:22:08 +0530

    changes for create rule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: '', component: DashboardComponent, pathMatch: 'full'},
-  {path: 'create-role', component: CreateRoleComponent},
+  {path: 'create-rule', component: CreateRoleComponent},
   { path: '**', redirectTo: '' }
 ];
 
